c8891f2bd8f99078d129267662c41e639ca6548b
devholic <me@devholic.io>
2017-06-04 15:03:02 +0900

    feat: add parseBody extension to ReqCtx

--- core/src/main/kotlin/io/devholic/restflux/core/context/Extension.kt
+++ core/src/main/kotlin/io/devholic/restflux/core/context/Extension.kt
@@ -5,10 +5,21 @@ import io.devholic.restflux.core.response.*
 import io.netty.buffer.ByteBuf
 import io.reactivex.netty.protocol.http.server.HttpServerResponse
 import rx.Observable
+import java.nio.charset.Charset
 import java.util.*
 
 
 private val restflux = "restflux"
+
+inline fun <reified T> RequestContext.parseBody(charset: Charset = Charsets.UTF_8): Observable<T> =
+    req.content
+        .map { it.toString(charset) }
+        .flatMap {
+            body ->
+            Observable.fromCallable {
+                RestfluxContext.gson.fromJson<T>(body, T::class.java)
+            }
+        }
 
 private fun HttpServerResponse<ByteBuf>.addDefaultHeader() {
     addDateHeader(Header.Date.value, Date())
