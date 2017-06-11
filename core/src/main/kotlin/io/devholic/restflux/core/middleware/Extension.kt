62f8ae343f5c50cbb4580890890c5e5c91bddfed
devholic <me@devholic.io>
2017-06-12 00:07:23 +0900

    fix: fix middleware concat logic

--- core/src/main/kotlin/io/devholic/restflux/core/middleware/Extension.kt
+++ core/src/main/kotlin/io/devholic/restflux/core/middleware/Extension.kt
@@ -5,17 +5,11 @@ import rx.Observable
 
 
 fun List<RestfluxMiddleware>.concat(): (Observable<RequestContext>) -> Observable<RequestContext> =
-    {
-        it.apply {
-            this@concat.forEach {
-                middleware ->
-                applyMiddleware(middleware)
-            }
-        }
-    }
+    { this@concat.fold(it, { acc, middleware -> acc.applyMiddleware(middleware) }) }
 
 fun Observable<RequestContext>.applyMiddleware(middleware: RestfluxMiddleware): Observable<RequestContext> =
     Observable.Transformer<RequestContext, RequestContext> {
-        it.flatMap { middleware.buildMiddlewareObservable(it) }
-            .filter { !it.isDone }
+        it.flatMap {
+            middleware.buildMiddlewareObservable(it)
+        }.filter { !it.isDone }
     }.call(this)
