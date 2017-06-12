b49ca1bdec092eef6007a3d8aa58cf19bca328ff
devholic <me@devholic.io>
2017-06-13 05:45:29 +0900

    refactor: remove useless `this` keyword

--- core/src/main/kotlin/io/devholic/restflux/core/middleware/Extension.kt
+++ core/src/main/kotlin/io/devholic/restflux/core/middleware/Extension.kt
@@ -5,7 +5,7 @@ import rx.Observable
 
 
 fun List<RestfluxMiddleware>.concat(): (Observable<RequestContext>) -> Observable<RequestContext> =
-    { this@concat.fold(it, { acc, middleware -> acc.applyMiddleware(middleware) }) }
+    { fold(it, { acc, middleware -> acc.applyMiddleware(middleware) }) }
 
 fun Observable<RequestContext>.applyMiddleware(middleware: RestfluxMiddleware): Observable<RequestContext> =
     Observable.Transformer<RequestContext, RequestContext> {
